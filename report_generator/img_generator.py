f308f4e111787ad59b5b99c73cc313cdf2d900ce
dst-evo <dominic.stalder@protonmail.ch>
2022-03-11 17:38:10 +0100

    created function to plot daily values in correct colour

--- report_generator/img_generator.py
+++ report_generator/img_generator.py
@@ -1,12 +1,25 @@
+from turtle import color
 import matplotlib.pyplot as plt
+import matplotlib as mpl
 import numpy as np
 import pandas as pd
+import datetime as dt
 
+# viridis discrete hex codes
+#   #FDE725 = L1 - VPM B (4958)
+#   #7AD151 = L2 - VPM A (4959)
+#   #22A884 = L3 - VPM C (4960)
+#   #2A788E = L4 - VPM D (4961)
+#   #414487 = Totals
+#
+#
 
 # print a single linechart
 # this is used for the following values:
 #   total produced boxes per day
 #   bad boxes per day
+
+
 def single_linechart():
     # create data
     values = np.cumsum(np.random.randn(1000, 1))
@@ -23,20 +36,29 @@ def single_linechart():
 #   total produced boxes per day and machine
 #   absolut bad boxes per day and machine
 #   relative bad boxes per day and machine
-def daily_barplot(bars):
-    # Make a random dataset:
-    height = [3, 12, 5, 18, 45]
-    #bars = ('A', 'B', 'C', 'D', 'E')
-    y_pos = np.arange(len(bars))
+def daily_barplot(height_b, height_a, height_c, height_d, date):
+    barWidth = 0.25
 
-    # Create bars
-    plt.bar(y_pos, height)
+    bar_b = np.arange(len(height_a))
+    bar_a = [x + barWidth for x in bar_b]
+    bar_c = [x + barWidth for x in bar_a]
+    bar_d = [x + barWidth for x in bar_c]
 
-    # Create names on the x-axis
-    plt.xticks(y_pos, bars)
+    plt.bar(bar_b, height_b, width=barWidth, label='L1 - VPM B (4958)',
+            color='#FDE725', edgecolor='white')
+    plt.bar(bar_a, height_a, width=barWidth, label='L2 - VPM A (4959)',
+            color='#7AD151', edgecolor='white')
+    plt.bar(bar_c, height_c, width=barWidth, label='L3 - VPM C (4960)',
+            color='#22A884', edgecolor='white')
+    plt.bar(bar_d, height_d, width=barWidth, label='L4 - VPM D (4961)',
+            color='#2A788E', edgecolor='white')
 
-    # Show graphic
-    plt.savefig('./output/img/daily_barplot.png')
+    plt.xlabel('date')
+    plt.xticks([r + barWidth for r in range(len(height_a))], date)
+
+    # save graphic
+
+    plt.savefig('./output/img/daily_barplot.svg')
 
 
 # barplot to display monthly averages for each machine
