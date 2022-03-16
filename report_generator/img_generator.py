d1e69e82fc66a2d3cdf4d75c19685958951aa0b7
dst-evo <dominic.stalder@protonmail.ch>
2022-03-16 16:40:47 +0100

    added function to plot linechart finished barplot function added rcParams for global Parameters

--- report_generator/img_generator.py
+++ report_generator/img_generator.py
@@ -1,6 +1,10 @@
 from turtle import color
+from typing import ValuesView
+from matplotlib import rcParams
 import matplotlib.pyplot as plt
 import matplotlib as mpl
+import matplotlib.dates as mdates
+from matplotlib.dates import DateFormatter
 import numpy as np
 import pandas as pd
 import datetime as dt
@@ -11,8 +15,17 @@ import datetime as dt
 #   #22A884 = L3 - VPM C (4960)
 #   #2A788E = L4 - VPM D (4961)
 #   #414487 = Totals
-#
-#
+
+# set global parameters
+plt.rcParams['figure.figsize'] = [16.0, 9.0]
+plt.rcParams['figure.dpi'] = 300
+plt.rcParams['font.family'] = 'Europa-Regular'
+plt.rcParams['axes.grid'] = True
+plt.rcParams['axes.grid.axis'] = 'y'
+plt.rcParams['axes.axisbelow'] = True
+plt.rcParams['legend.frameon'] = False
+plt.rcParams['legend.loc'] = 'upper center'
+plt.rcParams['lines.linewidth'] = 3.5
 
 # print a single linechart
 # this is used for the following values:
@@ -20,15 +33,36 @@ import datetime as dt
 #   bad boxes per day
 
 
-def single_linechart():
-    # create data
-    values = np.cumsum(np.random.randn(1000, 1))
+def single_linechart(val_b, val_a, val_c, val_d, date):
+    x = np.arange(len(date))
 
-    # plot data
-    plt.plot(values)
+    fig, ax = plt.subplots()
+    values = val_a + val_b.values + val_c.values + val_d.values
+    ax.plot(values, color='#414487')
+
+    ax.set_ylabel('Packages')
+    ax.set_ylim(bottom=0, top=35000)
+    box = ax.get_position()
+    ax.set_position([box.x0, box.y0 + box.height *
+                    0.1, box.width, box.height * 0.9])
+    ax.legend(['Total'], bbox_to_anchor=(0.5, -0.05), ncol=1)
+
+    day_locator = mdates.DayLocator()
+    day_month_formatter = mdates.DateFormatter(
+        "%d/%m")  # four digits for year, two for month
+    ax.xaxis.set_major_locator(day_locator)
+    # ax.xaxis.set_major_locator(mdates.AutoDateLocator())
+    # formatter for major axis only
+    ax.xaxis.set_major_formatter(day_month_formatter)
+
+    fig.tight_layout()
+    fig.autofmt_xdate()
+
+    plt.box(False)
+    plt.xticks(rotation=45)
 
     # save plot
-    plt.savefig('./output/img/single_linechart.png')
+    plt.savefig('./output/img/single_linechart.svg')
 
 
 # barplot to display daily values for each machine
@@ -36,30 +70,39 @@ def single_linechart():
 #   total produced boxes per day and machine
 #   absolut bad boxes per day and machine
 #   relative bad boxes per day and machine
-def daily_barplot(height_b, height_a, height_c, height_d, date):
-    barWidth = 0.25
+def daily_barplot(val_b, val_a, val_c, val_d, date):
+    x = np.arange(len(date))
+    barWidth = 0.2
 
-    bar_b = np.arange(len(height_a))
-    bar_a = [x + barWidth for x in bar_b]
-    bar_c = [x + barWidth for x in bar_a]
-    bar_d = [x + barWidth for x in bar_c]
+    fig, ax = plt.subplots()
+    bar_b = ax.bar(x-(barWidth*1.5), val_b, width=barWidth, label='L1 - VPM B (4958)',
+                   color='#FDE725')
+    bar_a = ax.bar(x-(barWidth*0.5), val_a, width=barWidth, label='L2 - VPM A (4959)',
+                   color='#7AD151')
+    bar_c = ax.bar(x+(barWidth*0.5), val_c, width=barWidth, label='L3 - VPM C (4960)',
+                   color='#22A884')
+    bar_d = ax.bar(x+(barWidth*1.5), val_d, width=barWidth, label='L4 - VPM D (4961)',
+                   color='#2A788E')
 
-    plt.bar(bar_b, height_b, width=barWidth, label='L1 - VPM B (4958)',
-            color='#FDE725', edgecolor='white')
-    plt.bar(bar_a, height_a, width=barWidth, label='L2 - VPM A (4959)',
-            color='#7AD151', edgecolor='white')
-    plt.bar(bar_c, height_c, width=barWidth, label='L3 - VPM C (4960)',
-            color='#22A884', edgecolor='white')
-    plt.bar(bar_d, height_d, width=barWidth, label='L4 - VPM D (4961)',
-            color='#2A788E', edgecolor='white')
+    ax.set_ylabel('Packages')
+    # ax.set_title('Daily Packages per Machine')
+    ax.set_ylim(bottom=0, top=10000)
 
-    plt.xlabel('date')
-    plt.xticks([r + barWidth for r in range(len(height_a))], date)
+    box = ax.get_position()
+    ax.set_position([box.x0, box.y0 + box.height *
+                    0.1, box.width, box.height * 0.9])
+    ax.legend(bbox_to_anchor=(0.5, -0.05), ncol=5)
 
-    # save graphic
+    ax.set_xticks(range(date.size))
+    ax.set_xticklabels([ts.strftime('%d/%m') if ts.year != date[idx-1].year
+                        else ts.strftime('%d/%m') for idx, ts in enumerate(date)])
 
+    fig.tight_layout()
+    fig.autofmt_xdate()
+
+    plt.box(False)
+    plt.xticks(rotation=45)
     plt.savefig('./output/img/daily_barplot.svg')
-
 
 # barplot to display monthly averages for each machine
 # this is used for the following values:
@@ -67,6 +110,8 @@ def daily_barplot(height_b, height_a, height_c, height_d, date):
 #   average produced boxes per hour per machine - CMC
 #   average relative boxes per machine
 #   average availability
+
+
 def monthly_barplot():
     # Make a random dataset:
     height = [3, 12, 5, 18, 45]
