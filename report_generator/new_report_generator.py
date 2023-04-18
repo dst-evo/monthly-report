8599222449c7abad97faf47986f078a8739ea215
Dominic Stalder <dominic.stalder@digitecgalaxus.ch>
2023-04-18 17:15:21 +0200

    some general formating and added the donut chart

--- report_generator/new_report_generator.py
+++ report_generator/new_report_generator.py
@@ -1,6 +1,6 @@
 # This is Version 2 of the famous Report Generator
 # The general Idea of the programm is the same as before
-# generating a comprehenisve Report about the current 
+# generating a comprehenisve Report about the current
 # Status of the Packing Machines used by Digitec Galaxus AG.
 # Most of the used data is from "Current Month - 1" but some
 # Values are compared month over month and year over year.
@@ -13,16 +13,16 @@
 # with help of some special people
 
 # load Libraries
-#------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 import matplotlib.colors as colors
 from matplotlib.lines import Line2D
-#------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
 
-# set global plt parameters 
-#------------------------------------------------------------------------------
+# set global plt parameters
+# ------------------------------------------------------------------------------
 plt.rcParams['figure.figsize'] = [16.0, 9.0]
 plt.rcParams['figure.dpi'] = 300
 plt.rcParams['font.family'] = 'Europa-Regular'
@@ -33,35 +33,56 @@ plt.rcParams['legend.frameon'] = False
 plt.rcParams['legend.loc'] = 'upper center'
 plt.rcParams['lines.linewidth'] = 3.5
 plt.rcParams['font.size'] = 22
-#------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
 
 # define colors and names for the plots
-#------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
 # define color map
 palette_lc = colors.ListedColormap(
-    ['#FDE725', '#7AD151', '#22A884', '#2A788E', '#414487', 'grey'])
+    ['#FDE725',
+     '#7AD151',
+     '#22A884',
+     '#2A788E',
+     '#414487',
+     'grey',
+     ])
 
-palette_list = (['#FDE725', '#7AD151', '#22A884',
-                '#2A788E', '#414487', 'grey'])
+palette_list = ['#FDE725',
+                '#7AD151',
+                '#22A884',
+                '#2A788E',
+                '#414487',
+                'grey',
+                ]
 
 # define legend style
 custom_lines = [Line2D([0], [0], color=palette_lc(0), lw=8),
                 Line2D([0], [0], color=palette_lc(1), lw=8),
                 Line2D([0], [0], color=palette_lc(2), lw=8),
-                Line2D([0], [0], color=palette_lc(3), lw=8)]
-machine_names = ['L1 - VPM B (4958)', 'L2 - VPM A (4959)',
-                 'L3 - VPM C (4960)', 'L4 - VPM D (4961)']
-#------------------------------------------------------------------------------
+                Line2D([0], [0], color=palette_lc(3), lw=8),
+                ]
+machine_names = ['L1 - VPM B (4958)',
+                 'L2 - VPM A (4959)',
+                 'L3 - VPM C (4960)',
+                 'L4 - VPM D (4961)',
+                 ]
+# ------------------------------------------------------------------------------
 
 # read csv files from local storage
-#------------------------------------------------------------------------------
-df_current = pd.read_csv('./raw_data/boxes_total_current.csv', delimiter=';', usecols=['DateTime', 'Machine', 'Boxes']) 
-df_previous = pd.read_csv('./raw_data/boxes_total_previous.csv', delimiter=';', usecols=['DateTime', 'Machine', 'Boxes'])
-#------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
+df_current = pd.read_csv('./raw_data/boxes_total_current.csv',
+                         delimiter=';',
+                         usecols=['DateTime', 'Machine', 'Boxes'],
+                         )
+df_previous = pd.read_csv('./raw_data/boxes_total_previous.csv',
+                          delimiter=';',
+                          usecols=['DateTime', 'Machine', 'Boxes'],
+                          )
+# ------------------------------------------------------------------------------
 
 
 # Plot total Boxes Year over Year
-#------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
 # Filter rows where Machine is 'Total'
 df_current = df_current[df_current['Machine'] == 'Total']
 df_previous = df_previous[df_previous['Machine'] == 'Total']
@@ -71,21 +92,25 @@ df_current['DateTime'] = pd.to_datetime(df_current['DateTime'])
 df_previous['DateTime'] = pd.to_datetime(df_previous['DateTime'])
 
 # Add a year to the DateTime column in df_previous
-df_previous['DateTime'] = df_previous['DateTime'] + pd.offsets.DateOffset(years=1)
+df_previous['DateTime'] = df_previous['DateTime'] + \
+    pd.offsets.DateOffset(years=1)
 
 # Merge the two dataframes based on DateTime column
 df_merged = pd.merge(df_current, df_previous, on='DateTime')
 
 # Rename the columns to differentiate between the two sets of boxes
-df_merged = df_merged.rename(columns={'Boxes_x': 'Boxes_current', 'Boxes_y': 'Boxes_previous'})
+df_merged = df_merged.rename(
+    columns={'Boxes_x': 'Boxes_current', 'Boxes_y': 'Boxes_previous'})
 
 # Extract the values for the desired time period
 start_date = pd.to_datetime('2022-04-01')
 end_date = pd.to_datetime('2023-03-01')
-df_period = df_merged[(df_merged['DateTime'] >= start_date) & (df_merged['DateTime'] < end_date)]
+df_period = df_merged[(df_merged['DateTime'] >= start_date)
+                      & (df_merged['DateTime'] < end_date)]
 
 # Group by month and year and sum Boxes values for each month and year
-df_period_monthly = df_period.groupby(pd.Grouper(key='DateTime', freq='M')).sum()
+df_period_monthly = df_period.groupby(
+    pd.Grouper(key='DateTime', freq='M')).sum()
 
 # Create a grouped bar chart comparing the boxes in each month
 labels = df_period_monthly.index.strftime('%b')
@@ -93,29 +118,124 @@ x = np.arange(len(labels))
 width = 0.35  # the width of the bars
 
 fig, ax = plt.subplots()
-rects1 = ax.bar(x - width/2, df_period_monthly['Boxes_current'], width, label='Current')
-rects2 = ax.bar(x + width/2, df_period_monthly['Boxes_previous'], width, label='Previous')
-
+# create the two bars
+rects1 = ax.bar(
+    x - width/2,
+    df_period_monthly['Boxes_previous'],
+    width,
+    color=palette_list[5],
+    label='Previous',
+)
+rects2 = ax.bar(
+    x + width/2,
+    df_period_monthly['Boxes_current'],
+    width,
+    color=palette_list[4],
+    label='Current',
+)
 # Add y-axis label and legend
 ax.set_ylabel('Boxes')
-ax.legend()
+box = ax.get_position()
+ax.set_position([box.x0,
+                 box.y0 + box.height * 0.2,
+                 box.width,
+                 box.height * 0.8],
+                )
+ax.legend(['Previous', 'Current'],
+          bbox_to_anchor=(0.5, -0.15),
+          ncol=2,
+          )
 
 # Add text labels to each column
 for rect in rects1 + rects2:
     height = rect.get_height()
-    ax.text(rect.get_x() + rect.get_width() / 2, height, int(height),
-            ha='center', va='bottom')
+    ax.text(rect.get_x() + rect.get_width() / 2,
+            height,
+            int(height),
+            ha='center',
+            va='bottom',
+            fontsize=16,
+            )
 
 # Set tick labels and title
 ax.set_xticks(x)
-ax.set_xticklabels(labels, rotation=45, ha='right')
-ax.set_title('Comparison of Boxes by Month and Year')
+ax.set_xticklabels(labels,
+                   rotation=45,
+                   ha='right',
+                   )
+ax.set_title('Comparison of Boxes by Month')
 
+plt.box(False)
 # Show the plot
+# plt.savefig('./test.pdf')
 plt.show()
 
 # TODO: save plot as pdf to use it in the report
+# ------------------------------------------------------------------------------
 
-#------------------------------------------------------------------------------
+# Make a donut Chart with all Machines and plot the total in the middle
+# ------------------------------------------------------------------------------
+# Filter the rows to include only the desired machines
+df_donut = pd.read_csv('./raw_data/boxes_total_current.csv',
+                       delimiter=';',
+                       usecols=['DateTime', 'Machine', 'Boxes'],
+                       )
 
+# Filter the rows to include only the last available month
+df_donut['DateTime'] = pd.to_datetime(df_donut['DateTime'])
+last_month = df_donut['DateTime'].max()
+df_last_month = df_donut[df_donut['DateTime'] == last_month]
 
+# Filter the rows to include only the desired machines
+df_machines = df_last_month[df_last_month['Machine'].isin(machine_names)]
+
+# Calculate the total number of boxes for the selected machines
+total_boxes = df_machines['Boxes'].sum()
+
+# Create a pie chart with the selected machines
+colors = [c for c in palette_list if c != 'grey']
+fig, ax = plt.subplots()
+wedges, texts, autotexts = ax.pie(df_machines['Boxes'],
+                                  labels=df_machines['Machine'],
+                                  colors=colors,
+                                  autopct='%1.1f%%',
+                                  startangle=90,
+                                  textprops={'fontsize': 14},
+                                  wedgeprops={'linewidth': 7,
+                                              'edgecolor': 'white',
+                                              }
+                                  )
+center_circle = plt.Circle((0, 0),
+                           0.5,
+                           color='white',
+                           )
+fig = plt.gcf()
+fig.gca().add_artist(center_circle)
+plt.setp(autotexts,
+         size=16,
+         weight="bold",
+         )
+plt.setp(texts,
+         size=16,
+         )
+
+# Add the total number of boxes to the center of the chart
+ax.text(0,
+        0,
+        total_boxes,
+        ha='center',
+        va='center',
+        fontsize=24,
+        )
+
+# Set the title of the chart
+ax.set_title('Total Boxes for Selected Machines ({})'.format(
+    last_month.strftime('%b %Y')),
+    fontsize=20,
+)
+
+# set axis equal to ensure circle is drawn as a circle
+plt.axis('equal')
+# Show the chart
+plt.show()
+# ------------------------------------------------------------------------------
