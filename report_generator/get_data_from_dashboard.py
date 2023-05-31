a26e20ba112eb92e63785ad6eab6b6f60fbf136a
Dominic Stalder <dominic.stalder@students.fhnw.ch>
2023-05-31 17:46:58 +0200

    removed a double time module import

--- report_generator/get_data_from_dashboard.py
+++ report_generator/get_data_from_dashboard.py
@@ -6,7 +6,6 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
-import time
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.firefox.options import Options
